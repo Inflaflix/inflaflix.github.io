dea892caac1e846ea0c8c407c4e1c281216b8874
Inflaflix <123211613+Inflaflix@users.noreply.github.com>
2023-02-26 16:25:33 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,207 +1,77 @@
-var videoPlaying = false;
-let r = document.querySelector(":root")
-
-var controlsActive = false;
-var controlsTimeout = 0;
-
-var seekingVideoIntervallID;
-var finderPercentage;
-var wasPlayed = false;
-var muted = false;
-var isInFullscreen = false;
-
-var mouseX;
-
-
-window.onload = function () {
-    setInterval(updateControls, 10);
-    setInterval(updateVideofinder, 100);
-}
+var previewToggled = false;
+var scrollHeight = 0;
+var lastScrollHeight = 0;
 
 function init() {
     let element;
     element = document.body;
 
-    element.addEventListener("mousemove", showControls);
+    scrollHeight = element.scrollTop;
 
-    var video = document.getElementById("video");
-    video.autoplay = true;
-    video.addEventListener("ended", videoEnded)
+    document.getElementById('preview-exit').addEventListener("click", togglePreview)
+    /*document.getElementById('preview-enter').addEventListener("click", togglePreview)*/
 
+    addEventListener("scroll", onBodyScroll)
 
-    playbuttonMode1 = document.getElementById("play-button-stopped");
-    playbuttonMode2 = document.getElementById("play-button-playing");
-    playbuttonMode1.style.setProperty("display", "block");
-    playbuttonMode2.style.setProperty("display", "none")
+    var previewbuttons = document.getElementsByClassName('info-panel-previewbutton');
+    for (i = 0; i < previewbuttons.length; i++) {
+        previewbuttons[i].addEventListener("click", togglePreview)
+        console.log("Added preview eventlistener to item " + i)
+    }
+    var playbuttons = document.getElementsByClassName('info-panel-playbutton');
+    for (i = 0; i < playbuttons.length; i++) {
+        /*playbuttons[i].addEventListener("click", togglePreview)
+        console.log("Added play eventlistener to item " + i)*/
+    }
 
-    var active = document.getElementById("sound-active");
-    var mute = document.getElementById("sound-mute");
-    active.style.setProperty("display", "block");
-    mute.style.setProperty("display", "none")
-
-    document.getElementById("playArea").addEventListener("click", toggleVideo)
-    document.getElementById("finder").addEventListener("mousedown", seekVideo)
-    document.getElementById("finder").addEventListener("mouseup", stopSeekingVideo)
-    document.addEventListener("fullscreenchange", function () { isInFullscreen = !isInFullscreen })
-    element.addEventListener("mouseup", stopSeekingVideo)
-    element.addEventListener("mousemove", updateMousePos)
-}
-
-function updateMousePos(event) {
-    mouseX = event.clientX;
+    
 }
 
 
-function toggleVideo() {
 
-    wasPlayed = true;
-    var video = document.getElementById("video");
-    playbuttonMode1 = document.getElementById("play-button-stopped");
-    playbuttonMode2 = document.getElementById("play-button-playing");
+function togglePreview(number) {
+    let element;
+    element = document.documentElement;
 
-    if (!videoPlaying) {
-        video.play();
-        playbuttonMode1.style.setProperty("display", "none");
-        playbuttonMode2.style.setProperty("display", "block")
-        videoPlaying = true;
-        console.log("playing now")
+    
+
+    if (previewToggled) {
+        previewToggled = false;
+        console.log(previewToggled + " " + element.scrollTop)
+        element.classList.toggle("preview-toggled")
+
+        element.scrollTop = scrollHeight;
+
     } else {
-        video.pause();
-        playbuttonMode1.style.setProperty("display", "block");
-        playbuttonMode2.style.setProperty("display", "none")
-        videoPlaying = false;
+        previewToggled = true;
+        console.log(previewToggled + " " + element.scrollTop)
+        scrollHeight = element.scrollTop;
+        element.scrollTop = 0;
+
+        let r = document.querySelector(":root")
+        r.style.setProperty("--scrollHeight", -scrollHeight + "px");
+
+        element.classList.toggle("preview-toggled")
     }
 }
 
-function videoEnded() {
-    videoPlaying = false
-    video.pause;
-}
+function onBodyScroll() {
+    body = document.documentElement;
+    thisScrollHeight = body.scrollTop;
 
-function triggerVideoSeeking() {
-    return window.setInterval(function () {
-        var pos = mouseX;
-        var finderBound = document.getElementById("finder").getBoundingClientRect();
-        var finderSize = finderBound.right - finderBound.left
-        finderPercentage = (pos - finderBound.left) / finderSize;
-        video.currentTime = video.duration * finderPercentage
-
-        console.log(finderPercentage)
-    }, 10);
-}
-
-function seekVideo() {
-    var pos = mouseX;
-    var finderBound = document.getElementById("finder").getBoundingClientRect();
-    var finderSize = finderBound.right - finderBound.left
-
-
-    seekingVideoIntervallID = triggerVideoSeeking();
-
-    console.log(pos + " " + finderSize);
-}
-
-function stopSeekingVideo() {
-    window.clearInterval(seekingVideoIntervallID);
-    console.log("Mouse up")
-}
-
-
-function updateVideofinder() {
-    var video = document.getElementById("video");
-    var time = video.currentTime;
-    var bufferedTime = video.buffered.end(0);
-    var videoLength = video.duration;
-
-    r.style.setProperty("--videoTime", ((time / videoLength) * 100) + "%");
-    r.style.setProperty("--videoBufferedTime", ((bufferedTime / videoLength) * 100) + "%");
-
-    if (time == videoLength) {
-        playbuttonMode1.style.setProperty("display", "block");
-        playbuttonMode2.style.setProperty("display", "none")
-        videoPlaying = false;
-    }
-}
-
-function updateControls() {
-    if (controlsActive && controlsTimeout > 0) {
-        controlsTimeout--;
+    if (thisScrollHeight > 0 && lastScrollHeight === 0) {
+        document.getElementById("nav-bar-container-background").classList.toggle("nav-bar-background-hidden");
+        console.log("toggled");
     }
 
-    if (controlsTimeout <= 0) {
-        controlsActive = false;
-        controlsTimeout = 0;
-        r.style.setProperty("--controlsVisible", 0);
-        r.style.setProperty("cursor", "none");
-
+    if (thisScrollHeight === 0 && lastScrollHeight > 0) {
+        document.getElementById("nav-bar-container-background").classList.toggle("nav-bar-background-hidden");
+        console.log("toggled");
     }
+
+    lastScrollHeight = thisScrollHeight;
 }
 
-function showControls() {
-    controlsActive = true;
-    controlsTimeout = 250;
-    r.style.setProperty("--controlsVisible", 1);
-    r.style.setProperty("cursor", "pointer");
-}
-
-function skip10() {
-    video.currentTime += 10;
-}
-
-function skipM10() {
-    video.currentTime -= 10;
-}
-
-function toggleMute() {
-    var active = document.getElementById("sound-active");
-    var mute = document.getElementById("sound-mute");
-
-    if (muted) {
-        video.muted = false;
-        muted = false;
-        active.style.setProperty("display", "block");
-        mute.style.setProperty("display", "none")
-    } else {
-        video.muted = true;
-        muted = true;
-        active.style.setProperty("display", "none");
-        mute.style.setProperty("display", "block")
-    }
-}
-
-function openFullscreen() {
-    var elem = document.documentElement;
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-    }
-}
-
-function closeFullscreen() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) { /* Safari */
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { /* IE11 */
-        document.msExitFullscreen();
-    }
-}
-
-function toggleFullscreen() {
-    if (!isInFullscreen) {
-        openFullscreen();
-    } else {
-        closeFullscreen();
-    }
-}
-
-function returnToMainPage() {
-    window.location.replace("../index.html");
-}
-
-function nextEpisode(number) {
-    window.location.replace("vidEp" + number + ".html");
+function playEpisode(number) {
+    window.location.replace("./videos/vidEp" + number + ".html");
 }
